f7ae6591de4c50e8389e407cb6edb02fe891ea21
lujingxuansc <lujingxuansc@163.com>
2019-03-23 17:17:40 +0800

    first time src

---
+++ src/test/java/com/imooc/order/repository/OrderMasterRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.imooc.order.repository;
+
+import com.imooc.order.OrderApplicationTests;
+import com.imooc.order.dataobject.OrderMaster;
+import com.imooc.order.enums.OrderStatusEnum;
+import com.imooc.order.enums.PayStatusEnum;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class OrderMasterRepositoryTest extends OrderApplicationTests {
+
+    @Autowired
+    private  OrderMasterRepository orderMasterRepository;
+
+    @Test
+    public void testSave(){
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234567");
+        orderMaster.setBuyerName("hinn");
+        orderMaster.setBuyerPhone("12222222");
+        orderMaster.setBuyerAddress("慕课网总部");
+        orderMaster.setBuyerOpenid("1101110");
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+
+        OrderMaster result = orderMasterRepository.save(orderMaster);
+        Assert.assertTrue(result!=null);
+    }
+}
